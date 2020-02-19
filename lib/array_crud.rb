174723ab150e61d844f182a964779eda24b8238e
Lydell Gaines <dasnic70@gmail.com>
2020-02-19 18:32:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  famous_cats = ["lil bub", "scratchy", "grumpy", "maru"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  famous_cats.push("Jerry")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  famous_cats.unshift("Simese")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  famous_cats[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  famous_cats[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  famous_cats[3]
 end
